3738ecb0701297aa7dcad2cc86afd7e0f317aac6
Stepan <sd.denisoff@gmail.com>
2019-03-29 13:41:51 +0300

    controller naming changes

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 api = Blueprint('api', __name__, template_folder='../templates', static_folder='../static')
 
 
-from app.api.controllers.IndexPageController import IndexPageController
+from app.api.controllers.index import IndexPageController
 api.add_url_rule('/', view_func=IndexPageController.as_view('IndexPage'))
